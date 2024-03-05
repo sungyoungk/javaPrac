0682fb3806d6d17d9e550f83791fe72f7bcaef05
Sung Young Kim <skim8700@gmail.com>
2024-03-06 01:09:31 +0900

    [자바의신] 7장-배열

---
+++ src/javaPrac/arrayPrac/ArrayInitialize.java
@@ -0,0 +1,12 @@
+package javaPrac.arrayPrac;
+
+public class ArrayInitialize {
+    public static void main(String[] args) {
+
+    }
+
+    public void otherInitialize(){
+        int[] lottoNumbers = {1, 2, 3, 4, 5};
+
+    }
+}
---
+++ src/javaPrac/arrayPrac/ArrayTwoDimension.java
@@ -0,0 +1,13 @@
+package javaPrac.arrayPrac;
+
+public class ArrayTwoDimension {
+    public static void main(String[] args) {
+        ArrayTwoDimension array = new ArrayTwoDimension();
+
+    }
+
+    public void twoDimensionArray(){}
+    int[][] twiDim = new int[2][3];
+
+
+}
