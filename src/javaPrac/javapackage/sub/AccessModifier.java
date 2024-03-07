97b5455e6ffd72c764f637ebd2228a392fd7fcae
Sung Young Kim <skim8700@gmail.com>
2024-03-07 15:37:24 +0900

    [자바의신] 9장 - 접근제어자

---
+++ src/javaPrac/javapackage/sub/AccessModifier.java
@@ -0,0 +1,19 @@
+package javaPrac.javapackage.sub;
+
+public class AccessModifier {
+     public void publicMethod() {
+
+     }
+
+     protected void protectedMethod() {
+
+     }
+
+     void packagePrivateMethod() {
+
+     }
+
+    private void privateMethod() {
+
+    }
+}
