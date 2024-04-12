f3c951b0abe4ae91e8d3ba2215ecad342d097de2
Sung Young Kim <skim8700@gmail.com>
2024-04-13 03:24:31 +0900

    자바의신 17장 annotation

--- src/javaPrac/inheritance/Parent.java
+++ src/javaPrac/inheritance/Parent.java
@@ -1,11 +1,12 @@
 package javaPrac.inheritance;
 
 public class Parent {
-//    public Parent() {
-//        System.out.println("Parent Constructor called");
-//    }
-
-    public void printName() {
-        System.out.println("parent - printName() Method called");
-    }
+     public Parent() {
+        System.out.println("Parent Constructor"); }
+        public Parent(String name) {
+         System.out.println("Parent(String) Constructor");
+        }
+        public void printName() {
+            System.out.println("printNameO - Parent");
+     }
 }
