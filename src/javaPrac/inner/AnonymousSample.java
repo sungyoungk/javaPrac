cf1f933e396cf4b647a4eb39e9e6372b6a959772
Sung Young Kim <skim8700@gmail.com>
2024-04-12 17:25:19 +0900

    자바의신 16장. 익명 클래스(nested, local)

--- src/javaPrac/inner/AnonymousSample.java
+++ src/javaPrac/inner/AnonymousSample.java
@@ -1,5 +1,7 @@
 package javaPrac.inner;
 
+import jdk.jfr.Event;
+
 public class AnonymousSample {
     public static void main(String[] args) {
         AnonymousSample sample = new AnonymousSample();
@@ -8,6 +10,29 @@ public class AnonymousSample {
     public void setButtonListener() {
         MagicButton button = new MagicButton();
         MagicButtonListener listener = new MagicButtonListener();
+        button.setListener(listener);
+        button.onClickProcess();
+    }
+
+    public void setButtonListenerAnomymous() {
+        MagicButton button = new MagicButton();
+        button.setListener(
+                new EventListener() {
+                    @Override
+                    public void onClick() {
+                        System.out.println("Magic Button Clicked!!!");
+                    }
+                }
+        );
+    }
+
+    public void setButtonListenerAnonymous() {
+        MagicButton button = new MagicButton();
+        EventListener listener = new EventListener() {
+            public void onClick() {
+                System.out.println("Magic Button Clicked");
+            }
+        };
         button.setListener(listener);
         button.onClickProcess();
     }
---
+++ src/javaPrac/inner/NestedValueReference.java
@@ -0,0 +1,46 @@
+package javaPrac.inner;
+
+import jdk.jfr.Event;
+
+public class NestedValueReference {
+
+    public int publicInt = 0;
+    protected int protectedInt = 1;
+    int justInt = 2;
+    private int privateInt = 3;
+    static int staticInt = 4;
+
+    static class StaticNested {
+        public void setValue() {
+            staticInt = 4;
+
+        }
+    }
+
+    class Inner {
+    public void setValue() {
+        publicInt = 20;
+        protectedInt = 21;
+        justInt = 22;
+        privateInt = 23;
+        staticInt = 24;
+    }
+    }
+
+    public void setValue() {
+        EventListener listener = new EventListener() {
+            @Override
+            public void onClick() {
+                publicInt = 30;
+                protectedInt=31;
+                justInt = 33;
+                privateInt=33;
+                staticInt= 34;
+            }
+        };
+    }
+
+
+
+
+}
