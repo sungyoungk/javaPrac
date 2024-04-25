2c7f113f16478f6e6adcf582097b8090078779d5
Sung Young Kim <skim8700@gmail.com>
2024-04-25 16:18:20 +0900

    [복습]자바의신 vol.2 3장 Generic

--- src/javaPrac/generic/WildcardSample.java
+++ src/javaPrac/generic/WildcardSample.java
@@ -3,26 +3,19 @@ package javaPrac.generic;
 public class WildcardSample {
     public static void main(String[] args) {
         WildcardSample sample = new WildcardSample();
-        sample.callWildcardMethod();
+
 
     }
 
-    public void callWildcardMethod() {
-        WildcardGeneric<String> wildcard = new WildcardGeneric<String>();
-        wildcard.setWildcard("A");
-        wildStringMethod(wildcard);
-        wildcardPramMethod(wildcard);
-    }
+public void callWildCardMethod() {
+    WildcardGeneric<String> wildcard = new WildcardGeneric<String>();
+    wildcard.setWildcard("A");
+    wildcardStringMethod(wildcard);
+}
 
-    public void wildStringMethod(WildcardGeneric<String> c) {
-        String value = c.getWildcard();
-        System.out.println(value);
-    }
-
-    public void wildcardPramMethod(WildcardGeneric<?> c) {
+    public void wildcardStringMethod(WildcardGeneric<?> c) {
         Object value = c.getWildcard();
-        if (value instanceof String) {
-            System.out.println(value);
-        }
+        if (c.getWildcard() instanceof String)  System.out.println(value);
+
     }
 }
