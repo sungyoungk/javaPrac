e7ff8cb58802409aa5e20ae407429ccd67c6636d
sungyoung.kim <sungyoung.kim@medisolution.o.kr>
2024-06-18 17:07:20 +0900

    자바의신 vol2. Java8-Optional p333

---
+++ src/javaPrac/defaultMethod/PreviousInterface.java
@@ -0,0 +1,9 @@
+package javaPrac.defaultMethod;
+
+public interface PreviousInterface {
+    static final String name = "GodOfJavaBook";
+    static final int since = 2013;
+
+    String getName();
+    int getSince();
+}
