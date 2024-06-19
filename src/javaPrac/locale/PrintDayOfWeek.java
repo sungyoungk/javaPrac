422f4b9feb66a0f06caf6c950bc5305fe7de18e0
sungyoung.kim <sungyoung.kim@medisolution.o.kr>
2024-06-19 10:56:09 +0900

    자바의신 vol2. Java8-StringJoiner p339

--- src/javaPrac/locale/PrintDayOfWeek.java
+++ src/javaPrac/locale/PrintDayOfWeek.java
@@ -1,2 +1,37 @@
-package javaPrac.locale;public class PrintDayOfWeek {
+package javaPrac.locale;
+
+import java.time.DayOfWeek;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+public class PrintDayOfWeek {
+    public static void main(String[] args) {
+        PrintDayOfWeek sample = new PrintDayOfWeek();
+        // sample.printDayOfWeek();
+        sample.printDayOfWeekOfLocales();
+    }
+
+    public void printDayOfWeek() {
+        DayOfWeek[] dayOfWeeks = DayOfWeek.values();
+        Locale locale = Locale.getDefault();
+
+        for(DayOfWeek day : dayOfWeeks){
+            System.out.println(day.getDisplayName(TextStyle.FULL, locale));
+            System.out.println(day.getDisplayName(TextStyle.SHORT, locale));
+            System.out.println(day.getDisplayName(TextStyle.NARROW, locale));
+        }
+    }
+
+    public void printDayOfWeekOfLocales(){
+        DayOfWeek day = DayOfWeek.SUNDAY;
+        Locale[] locales = Locale.getAvailableLocales();
+
+        for(Locale locale : locales){
+            System.out.print(locale.getCountry() + " ");
+            System.out.print(day.getDisplayName(TextStyle.FULL, locale) + " ");
+            System.out.print(day.getDisplayName(TextStyle.SHORT, locale) + " ");
+            System.out.println(day.getDisplayName(TextStyle.NARROW, locale) + " ");
+        }
+    }
+
 }
