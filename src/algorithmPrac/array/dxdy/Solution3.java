bd7d690fc20b7d791e6957f4e0177ab8f476f2ad
sungyoung.kim <sungyoung.kim@medisolution.o.kr>
2024-09-02 17:37:26 +0900

    dx, dy 아직도 어려운 알고리즘의 세계

--- src/algorithmPrac/array/dxdy/Solution3.java
+++ src/algorithmPrac/array/dxdy/Solution3.java
@@ -2,25 +2,25 @@ package algorithmPrac.array.dxdy;
 
 import java.util.Arrays;
 
-public class Solution2 {
+public class Solution3 {
     private static final int[] dx = {0, 1, -1};
     private static final int[] dy = {1, 0, -1};
 
 
     public int[] solution(int n) {
         int[][] triangle = new int[n][n];
-        int v = 1;
         int x = 0;
         int y = 0;
         int d = 0;
+        int v = 1;
 
         while (true) {
             triangle[y][x] = v++;
             int nx = x + dx[d];
             int ny = y + dy[d];
-            if(nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) {
-              d = (d+1) % 3;
 
+            if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) {
+                d = (d + 1) % 3;
                 nx = x + dx[d];
                 ny = y + dy[d];
                 if(nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) break;
@@ -34,17 +34,17 @@ public class Solution2 {
             for (int j = 0; j <= i; j++) {
                 result[index++] = triangle[i][j];
             }
-        }
-        return result;
     }
+        return result;
+}
 
     public static void main(String[] args) {
-        Solution2 solution = new Solution2();
+        Solution3 solution = new Solution3();
         int[] answer = solution.solution(6);
-
         System.out.println(Arrays.toString(answer));
 
     }
+
 
 
 }
