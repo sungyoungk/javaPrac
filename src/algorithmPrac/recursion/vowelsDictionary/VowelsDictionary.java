aefb8cd3f07d9fedc17c2741a1517c11c6dc8769
sungyoung.kim <sungyoung.kim@medisolution.o.kr>
2024-11-04 17:42:12 +0900

    [시작] 모음사전 level2 p.180
    https://school.programmers.co.kr/learn/courses/30/lessons/84512

---
+++ src/algorithmPrac/recursion/vowelsDictionary/VowelsDictionary.java
@@ -0,0 +1,15 @@
+package algorithmPrac.recursion.vowelsDictionary;
+
+/***
+ * @모음_사전 (Level 2) p.180
+ * @URL: https://school.programmers.co.kr/learn/courses/30/lessons/84512
+ */
+
+public class VowelsDictionary {
+
+
+
+    public int solution(String word) {
+        return 1;
+    }
+}
