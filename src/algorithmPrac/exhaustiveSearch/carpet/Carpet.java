98d17c04dbf6d78d6d80a4ed865c686e5edf65e0
sungyoung.kim <sungyoung.kim@medisolution.o.kr>
2024-11-07 17:29:37 +0900

    [시작] 카펫 level2 p.198
    /**
     * @카펫_level2 p.198
     * @URL: https://school.programmers.co.kr/learn/courses/30/lessons/42842
     */
    public class Carpet {
    
        int[] solution(int brown, int yellow) {
    
            return new int[]{0,0,};
        }
    
    }

---
+++ src/algorithmPrac/exhaustiveSearch/carpet/Carpet.java
@@ -0,0 +1,14 @@
+package algorithmPrac.exhaustiveSearch.carpet;
+
+/**
+ * @카펫_level2 p.198
+ * @URL: https://school.programmers.co.kr/learn/courses/30/lessons/42842
+ */
+public class Carpet {
+
+    int[] solution(int brown, int yellow) {
+
+        return new int[]{0,0,};
+    }
+
+}
