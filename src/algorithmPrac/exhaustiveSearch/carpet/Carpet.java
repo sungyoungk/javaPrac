9db58cd51e99fc624f72b5ebcfed531e7d568d42
sungyoung.kim <sungyoung.kim@medisolution.o.kr>
2024-11-08 16:22:12 +0900

    [진행중] 모의고사 level1 p.192
    - 오! 일단 풀이 한 번 읽어보고 이해하니까 한번에 풀었음
    - 넓이화 길이를 이해하는 수학적 사고력이 조금 요구되는거 같음 ㅜㅜ

--- src/algorithmPrac/exhaustiveSearch/carpet/Carpet.java
+++ src/algorithmPrac/exhaustiveSearch/carpet/Carpet.java
@@ -6,9 +6,34 @@ package algorithmPrac.exhaustiveSearch.carpet;
  */
 public class Carpet {
 
+
+    // int boundary = (width + height -2) * 2
+    // int center = (width * height) - boundary
+    // O O O O
+    // O X X O
+    // O O O O
+
+
+//    제한사항
+//   • 갈색 격자의 수 brown은 8 이상 5,000 이하인 자연수입니다.
+//   • 노란색 격자의 수 y이low는 1 이상 2,000,000 이하인 자연수입니다.
+//   • 카펫의 가로 길이는 세로 길이와 같거나, 세로 길이보다 깁니다.
+
+
+
     int[] solution(int brown, int yellow) {
 
-        return new int[]{0,0,};
+        for ( int width = 3; width <= 5000; width++) {
+            for( int height = 3; height <= width; height ++) {
+                int boundary = (width + height -2) * 2;
+                int center = (width * height) - boundary;
+
+                if(boundary == brown && center == yellow) {
+                    return new int[]{width, height};
+                }
+            }
+        }
+        return null;
     }
 
 }
