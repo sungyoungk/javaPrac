f673a35cf7b867e3be28b4aaad0e0d68c614992e
sungyoung.kim <sungyoung.kim@medisolution.o.kr>
2024-11-12 11:08:57 +0900

    [시작] 소수찾기 level2 p.202
    - 종료 조건 점화식 혼자 어떻게 도출함?

---
+++ src/algorithmPrac/exhaustiveSearch/primeNumber/PrimeNumber.java
@@ -0,0 +1,14 @@
+package algorithmPrac.exhaustiveSearch.primeNumber;
+
+/**
+ * @소수찾기 level2 p.211
+ * @URL:  https://school.programmers.co.kr/learn/courses/30/lessons/42839
+ */
+public class PrimeNumber {
+
+    public int solution(String numbers) {
+        int answer = 0;
+        return answer;
+    }
+
+}
