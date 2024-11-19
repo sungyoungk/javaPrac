8e08245e906291ad738832da43e1e2be39e95c64
sungyoung.kim <sungyoung.kim@medisolution.o.kr>
2024-11-19 13:49:42 +0900

    두 개 뽑아서 더하기 level1 p. 240
    /**
     * @두_개_뽑아서_더하기 level1 p. 240
     * @URL: https://school.programmers.co.kr/learn/courses/30/lessons/68644
     *
    
     정수 배열 numbers가 주어집니다.
     numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들
     수 있는 모든 수를 배열에 오름차순으로 담아 return하도록
      solution 함수를 완성해주세요.
    
    [입출력 예시]
    |_______________________________
       numbers    |  result
    |-------------------------------
     [2,1,3,4,1]  |  [2,3,4,5,6,7]
    |-------------------------------
    
     *
     */

---
+++ src/algorithmPrac/sort/pairSum/PairSum.java
@@ -0,0 +1,25 @@
+package algorithmPrac.sort.pairSum;
+
+/**
+ * @두_개_뽑아서_더하기 level1 p. 240
+ * @URL: https://school.programmers.co.kr/learn/courses/30/lessons/68644
+ *
+
+ 정수 배열 numbers가 주어집니다.
+ numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들
+ 수 있는 모든 수를 배열에 오름차순으로 담아 return하도록
+  solution 함수를 완성해주세요.
+
+[입출력 예시]
+|_______________________________
+   numbers    |  result
+|-------------------------------
+ [2,1,3,4,1]  |  [2,3,4,5,6,7]
+|-------------------------------
+
+ *
+ */
+public class PairSum {
+
+
+}
