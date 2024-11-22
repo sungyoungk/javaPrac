ebd11f50594b7f507adb187b18945c0645b7285e
sungyoung.kim <sungyoung.kim@medisolution.o.kr>
2024-11-22 11:17:05 +0900

    [시작] H-Index level2 p.243

---
+++ src/algorithmPrac/sort/hIndex/HIndex.java
@@ -0,0 +1,22 @@
+package algorithmPrac.sort.hIndex;
+
+/**
+ * @H-Index level 2 p.243
+ * @URL:  https://school.programmers.co.kr/learn/courses/30/lessons/42747
+ */
+
+/**@문제
+ *    어떤 과학자가 발표한 논문 n편 중,
+ *    h번 이상 인용된 논문이 h편 이상이고 나머지 논문이 h번 이하 인용되었다면 h의 최댓값이 이 과학자의 H-lndex 이다.
+ *    어떤 과학자가 발표한 논문의 인용 횟수를 담은 배열 citations가 매개변수로 주어질 때,
+ *    이 과학자의 H-lndex 를 return 하도록 solution 함수를 작성해주세요.
+ */
+
+public class HIndex {
+
+    public int solution(int[] citations) {
+        int answer = 0;
+        return answer;
+    }
+
+}
