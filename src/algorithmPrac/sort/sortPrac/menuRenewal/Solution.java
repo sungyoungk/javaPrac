9c8ec2ef831551dc264d9d4b0790d891804dad78
sungyoung.kim <sungyoung.kim@medisolution.o.kr>
2024-12-02 09:55:51 +0900

    [시작] 리뉴얼_level_2 p.260
    /**
     * @메뉴_리뉴얼_level_2
     * @URL: https://school.programmers.co.kr/learn/courses/30/lessons/72411
     */
    public class Solution {
        public String[] solution(String[] orders, int[] course) {
            String[] answer = {};
            return answer;
        }
    }

---
+++ src/algorithmPrac/sort/sortPrac/menuRenewal/Solution.java
@@ -0,0 +1,12 @@
+package algorithmPrac.sort.sortPrac.menuRenewal;
+
+/**
+ * @메뉴_리뉴얼_level_2
+ * @URL: https://school.programmers.co.kr/learn/courses/30/lessons/72411
+ */
+public class Solution {
+    public String[] solution(String[] orders, int[] course) {
+        String[] answer = {};
+        return answer;
+    }
+}
