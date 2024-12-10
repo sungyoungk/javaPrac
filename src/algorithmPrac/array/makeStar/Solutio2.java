b855f5cf3f28a66b2310846f12b3d237d4bbd3e5
sungyoung.kim <sungyoung.kim@medisolution.o.kr>
2024-12-10 17:36:58 +0900

    [복습]교점에_별_만들기 level_2 p.60 - 좌표변환 주의!!!
    - 외우긴 한 것 같음?

--- src/algorithmPrac/array/makeStar/Solutio2.java
+++ src/algorithmPrac/array/makeStar/Solutio2.java
@@ -20,67 +20,67 @@ public class Solutio2 {
         }
     }
 
-    private Point intersection(long A, long B, long E, long C, long D, long F ) {
-        double x = (double) (B*F - E*D) / (A*D - B*C);
-        double y = (double) (E*C - A*F) / (A*D - B*C);
+    private Point getMaxPoint(List<Point> points) {
+        long x = Long.MIN_VALUE ;
+        long y = Long.MIN_VALUE ;
+        for(Point p : points) {
+           if(p.x > x) x = p.x ;
+           if(p.y > y) y = p.y ;
+        }
+        return new Point(x, y) ;
+    }
+
+    private Point getMinPoint(List<Point> points) {
+        long x = Long.MAX_VALUE ;
+        long y = Long.MAX_VALUE ;
+
+        for(Point p : points) {
+            if(p.x < x) x = p.x ;
+            if(p.y < y) y = p.y ;
+        }
+        return new Point( x, y) ;
+    }
+
+    private Point intersection(long A, long B, long E, long C, long D, long F) {
+
+        double x = (double) (B*F - E*D) / (A*D - B*C) ;
+        double y = (double) (E*C - A*F) / (A*D - B*C) ;
 
         if(x % 1 != 0 || y % 1 != 0) return null;
         return new Point((long)x, (long)y);
     }
 
-    private Point max(List<Point> points) {
-        long x = Long.MIN_VALUE;
-        long y = Long.MIN_VALUE;
-
-        for(Point p : points) {
-            if(p.x > x) x = p.x;
-            if(p.y > y) y = p.y;
-        }
-        return new Point(x, y);
-    }
-
-    private Point min(List<Point> points) {
-        long x = Long.MAX_VALUE;
-        long y = Long.MAX_VALUE;
-
-        for(Point p : points) {
-            if(p.x < x) x = p.x;
-            if(p.y < y) y = p.y;
-        }
-        return new Point(x, y);
-    }
-
     public String[] solution(int[][] line) {
-
-        List<Point> points = new ArrayList<>();
-        for(int i = 0; i < line.length; i++) {
-            for(int j = 0; j < line.length; j++) {
-                Point intersection = intersection(line[i][0], line[i][1], line[i][2], line[j][0], line[j][1], line[j][2]);
-                if(intersection != null) points.add(intersection);
-            }
-        }
-
-        Point max = max(points);
-        Point min = min(points);
-
-        int width  = (int)(max.x - min.x + 1);
-        int height = (int)(max.y - min.y + 1);
-
-        char[][] arr = new char[height][width];
-        for(char[] c : arr) {
-        Arrays.fill(c, '.');
-        }
-
-        for(Point p : points) {
-            int x = (int) (p.x - min.x);
-            int y = (int) (max.y - p.y);
-            arr[y][x] = '*';
-        }
-
-        String[] result = new String[arr.length];
-       for(int i = 0; i < result.length; i ++) {
-           result[i] = new String(arr[i]);
+    List<Point> points = new ArrayList<>();
+       for(int i = 0; i < line.length; i++) {
+           for(int j = 0; j < line.length; j++) {
+               Point intersection = intersection(line[i][0], line[i][1], line[i][2], line[j][0], line[j][1], line[j][2]);
+               if(intersection != null) {
+                   points.add(intersection) ; // 교점들 좌표 모두 저장!
+               }
+           }
        }
-       return result;
+       Point max = getMaxPoint(points) ;
+       Point min = getMinPoint(points) ;
+
+       int width  = (int) (max.x - min.x + 1) ; // 최대 최소
+       int height = (int) (max.y - min.y + 1) ; //
+
+        char[][] arr = new char[height][width] ;
+        for(char[] c : arr) {
+            Arrays.fill(c, '.') ;
+        } // 모든 영역에 . 표시
+
+        for ( int i = 0; i < points.size(); i ++) {
+            int x = (int)(points.get(i).x - min.x) ;
+            int y = (int)(max.y - points.get(i).y) ;
+            arr[y][x] = '*' ;
+        }
+
+        String[] result = new String[arr.length] ;
+        for( int i = 0; i < result.length; i ++) {
+            result[i] = new String(arr[i]) ;
+        }
+        return result ;
     }
 }
