a82ffa8f5a2de9cc33b1b533b943ec7205352f3d
sungyoung.kim <sungyoung.kim@medisolution.o.kr>
2024-12-20 17:49:20 +0900

    [복습]삼각_달팽이 level_2 p.72 (dx, dy) - if 절  조건을 잘 보자

---
+++ src/algorithmPrac/array/triagleSnail/dxdy/Solution.java
@@ -0,0 +1,42 @@
+package algorithmPrac.array.triagleSnail.dxdy;
+
+/**
+ * @삼각_달팽이 Level2(P.72)
+ * @URL: https://school.programmers.co.kr/learn/courses/30/lessons/68645
+ */
+public class Solution {
+    //                       하  우   좌
+    private final int[] dx = {0, 1, -1};
+    private final int[] dy = {1, 0, -1};
+
+    public int[] solution(int n) {
+    int[][] triangle = new int[n][n];
+    int v = 1;
+    int x = 0;
+    int y = 0;
+    int d = 0;
+
+    while(true) {
+        triangle[y][x] = v++;
+        int nx = x + dx[d];
+        int ny = y + dy[d];
+        if(nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) {
+            d = (d + 1) % 3;
+            nx = x + dx[d];
+            ny = y + dy[d];
+            if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) break;
+        }
+        x = nx;
+        y = ny;
+    }
+
+    int[] result = new int[v-1];
+    int index = 0;
+    for(int i = 0; i< n; i++) {
+        for(int j = 0; j <= i; j++) {
+            result[index++] = triangle[i][j];
+        }
+    }
+        return result;
+    }
+}
