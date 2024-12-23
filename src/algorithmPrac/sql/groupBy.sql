e6a5c280c0144fe2b097de07cbd423314356bf67
sungyoung.kim <sungyoung.kim@medisolution.o.kr>
2024-12-23 10:53:45 +0900

    [sql연습] 프로그래머스 sql 고득점 kit

--- src/algorithmPrac/sql/groupBy.sql
+++ src/algorithmPrac/sql/groupBy.sql
@@ -14,3 +14,15 @@ where price = (
   and category in ( '과자', '국', '김치', '식용유')
 group by a.category
 order by a.price desc
+
+/***
+  @자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기
+  @URL: https://school.programmers.co.kr/learn/courses/30/lessons/157340
+ */
+SELECT CAR_ID AS CAR_ID
+     , CASE
+           WHEN MAX('2022-10-16' BETWEEN START_DATE AND END_DATE) THEN '대여중'
+           ELSE '대여 가능' END AS AVAILABILITY
+from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+group by CAR_ID
+order by CAR_ID DESC
