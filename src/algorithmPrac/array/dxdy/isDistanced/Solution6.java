c06a2494e09fd2d2ec8c12d4490f1d9a10c7b7b1
sungyoung.kim <sungyoung.kim@medisolution.o.kr>
2025-01-06 14:47:25 +0900

    [복습]  프로그래머스_거리두기 확인하기(Lv2) p.96

--- src/algorithmPrac/array/dxdy/isDistanced/Solution6.java
+++ src/algorithmPrac/array/dxdy/isDistanced/Solution6.java
@@ -12,39 +12,36 @@ public class Solution6 {
     private final int[] dx = {0, -1, 1, 0};
     private final int[] dy = {-1, 0, 0, 1};
 
-
-    private boolean isNextVolunteer(char[][] room, int x, int y, int exclude) {
-            for (int d = 0; d < 4; d++) {
-                if ( d == exclude) continue;
-
-                int nx = x + dx[d];
-                int ny = y + dy[d];
-
-                if(ny < 0 || ny >= room.length || nx < 0 || nx >= room.length) continue;
-                if( room[ny][nx] == 'P' ) return true;
+    private boolean isNext(char[][] room, int x, int y, int exclude) {
+        for(int d = 0; d < 4; d++) {
+            if (d == exclude) continue;
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if(ny < 0 || ny >= room.length || nx < 0 || nx >= room[ny].length) continue;
+            if(room[ny][nx] == 'P') return true;
         }
-            return false;
+        return false;
     }
 
-    private boolean distanced(char[][] room , int x, int y) {
+    private boolean distanced(char[][] room, int x, int y) {
         for(int d = 0; d < 4; d++) {
             int nx = x + dx[d];
             int ny = y + dy[d];
             if(ny < 0 || ny >= room.length || nx < 0 || nx >= room[ny].length) continue;
-            switch(room[ny][nx]) {
-                case 'P' : return false;
-                case 'O' : if(isNextVolunteer(room, x, y, 3-d)) return false;
-                break;
-            }
+           switch(room[ny][nx]) {
+               case 'P' : return false;
+               case 'O' : if(isNext(room, nx, ny, 3 - d)) return false;
+               break;
+           }
         }
         return true;
     }
 
     private boolean distanced(char[][] room) {
-        for ( int y = 0; y < room.length; y++) {
+        for(int y = 0; y < room.length; y++) {
             for( int x = 0; x < room[y].length; x++) {
                 if(room[y][x] != 'P') continue;
-                if(!distanced(room, x, y)) return false;
+                if(! distanced(room, x, y)) return false;
             }
         }
         return true;
@@ -52,15 +49,14 @@ public class Solution6 {
 
     public int[] solution(String[][] places) {
         int[] answer = new int[places.length];
-
-
-        for (int i = 0; i < places.length; i++) {
+        for(int i = 0; i < answer.length; i++) {
             String[] place = places[i];
-            char[][] room = new char[places.length][];
-            for (int j = 0; j < place.length; j++) {
+            char[][] room = new char[place.length][];
+
+            for(int j = 0; j < room.length; j++) {
                 room[j] = place[j].toCharArray();
             }
-            if (distanced(room)) {
+            if(distanced(room)) {
                 answer[i] = 1;
             } else {
                 answer[i] = 0;
