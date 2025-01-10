4c3be9ba594dcd7f30721ea686b8194e1162222c
sungyoung.kim <sungyoung.kim@medisolution.o.kr>
2025-01-10 11:47:54 +0900

    [복습] 행렬의 곱셈

--- src/algorithmPrac/array/matrix/Solution3.java
+++ src/algorithmPrac/array/matrix/Solution3.java
@@ -13,15 +13,14 @@ public class Solution3 {
 
     public int[][] solution(int[][] arr1, int[][] arr2) {
         int[][] arr = new int[arr1.length][arr2[0].length];
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
+        for(int i = 0; i < arr1.length; i++) {
+            for(int j = 0; j < arr2[0].length; j++) {
                 arr[i][j] = 0;
-                for (int k = 0; k < arr1[i].length; k++) {
-                    arr[i][j] += arr1[i][k] * arr2[k][j];
-                }
+            for(int k = 0; k < arr[i].length; k++) {
+                arr[i][j] += arr[i][k] * arr[k][i];
+            }
             }
         }
         return arr;
     }
-
 }
