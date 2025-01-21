8a92581ad04d2aec38b0848a76fd50cc8ad77a4b
sungyoung.kim <sungyoung.kim@medisolution.o.kr>
2025-01-21 17:01:57 +0900

    [복습] 시저암호 - 완전 내것 아닌 느낌

--- src/algorithmPrac/string/CaesarPassword2.java
+++ src/algorithmPrac/string/CaesarPassword2.java
@@ -7,11 +7,12 @@ public class CaesarPassword2 {
      */
 
     private char push(char c, int n) {
-       if(!Character.isAlphabetic(c)) return c;
-       int offset = (Character.isUpperCase(c)) ? 'A' : 'a';
-       int position = (c - offset);
-       position = (position + n) % ('Z' - 'A' + 1);
-       return (char) (offset + position);
+        if(!Character.isAlphabetic(c)) return c;
+
+        int offset = (Character.isUpperCase(c)) ? 'A' : 'a';
+        int position = c - offset;
+        position = (char)(position + n) % ('Z' - 'A' + 1);
+        return (char)(offset + position);
     }
 
     public String solution(String str, int n) {
