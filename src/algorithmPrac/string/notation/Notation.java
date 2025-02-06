2000bd541c5f13fe2020c6a70f95a7888c8542b4
sungyoung.kim <sungyoung.kim@medisolution.o.kr>
2025-02-06 11:31:13 +0900

    [복습] @3진법_뒤집기_level1 - p.128 - return 할때도 3진법 수를 리턴해야!!

--- src/algorithmPrac/string/notation/Notation.java
+++ src/algorithmPrac/string/notation/Notation.java
@@ -5,25 +5,26 @@ public class Notation {
      *  @URL: https://programmers.co.kr/learn/courses/30/lessons/68935
      */
 
+    // 자연수 n이 매개변수로 주어집니다.
+    // n을 3진법 상에서 앞뒤로 뒤집은 후,
+    // 이를 다시 10진법으로 표현한 수를 return
 
     int n = 45;
 
 
     public long solution(int n) {
+        String reversed = new StringBuilder(Integer.toString(n, 3))
+                .reverse()
+                .toString();
 
-      String num = Long.toString(n, 3);
-      String reversed = new StringBuilder(num).reverse().toString();
-
-      return Long.parseLong(reversed, 3);
-
+        return (long) Integer
+                .parseInt(reversed, 3);
     }
 
     public static void main(String[] args) {
-
         int n = 45;
 
         Notation notation = new Notation();
         System.out.println(notation.solution(n));
     }
-
 }
