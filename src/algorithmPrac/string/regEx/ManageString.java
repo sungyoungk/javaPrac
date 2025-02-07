e863eddda4e3683aaf67c52ed28c9221594b7435
sungyoung.kim <sungyoung.kim@medisolution.o.kr>
2025-02-07 17:06:18 +0900

    문자열_다루기_기본_level1 (p.147)
        /**
         * @문자열_다루기_기본_level1 (p.147)
         * @URL: https://programmers.co.kr/learn/courses/30/lessons/12918
         */

--- src/algorithmPrac/string/regEx/ManageString.java
+++ src/algorithmPrac/string/regEx/ManageString.java
@@ -12,7 +12,7 @@ public class ManageString {
 
     public boolean solution(String s) {
 
-        return (s.matches("^[0-9]{4}") || s.matches("^[0-9]{6}")) ;
+    return s.matches("^[0-9]{4}$|^[0-9]{6}$");
 
     }
 
