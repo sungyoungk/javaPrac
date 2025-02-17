c64e01e74c78b9079d0800778eedb24e700525f2
sungyoung.kim <sungyoung.kim@medisolution.o.kr>
2025-02-17 14:24:56 +0900

    하노이의_탑 level 3 p.172 - 숙지 부족

--- src/algorithmPrac/recursion/towerOfHanoi/Solution.java
+++ src/algorithmPrac/recursion/towerOfHanoi/Solution.java
@@ -19,24 +19,21 @@ public class Solution {
          {n-1, from, empty}
          */
 
-        private void hanoi(int n, int from, int to , List<int[]>process) {
-            if (n == 1) {
-                process.add(new int[]{from, to});
-                return;
-            }
 
+        private List<int[]> hanoi(int n, int from, int to) {
             int empty = 6 - from - to;
+            List<int[]> list = new ArrayList<>();
+            if (n == 1) return List.of(new int[] {from, to});
 
-            hanoi(n-1, from, empty, process);
-            hanoi(1, from, to, process);
-            hanoi(n-1, empty, to, process);
+            list.addAll(hanoi(n-1, from, empty));
+            list.addAll(hanoi(1, from, to));
+            list.addAll(hanoi(n-1, empty, to));
+            return list;
         }
 
         public int[][] solution(int n) {
-            List<int[]> process = new ArrayList<>();
-            hanoi(n, 1, 3, process);
 
-            return process.toArray(new int[0][]);
+            return hanoi(n,1, 3).toArray(new int[0][]);
         }
     }
 }
