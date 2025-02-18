a1dc1a0d3ddd27a9f9387f4d23809ff2bbebbbb6
sungyoung.kim <sungyoung.kim@medisolution.o.kr>
2025-02-18 10:05:50 +0900

    [복습] 하노이의탑

--- src/algorithmPrac/recursion/towerOfHanoi/Hanoi3.java
+++ src/algorithmPrac/recursion/towerOfHanoi/Hanoi3.java
@@ -18,11 +18,12 @@ public class Hanoi3 {
      {n-1, from, empty}
      */
 
-    private List<int[]> hanoi(int n, int from, int to) {
-        if (n == 1) return List.of(new int[]{from, to});
+    private List<int[]>hanoi(int n, int from, int to) {
+        List<int[]> result = new ArrayList<>();
+
+        if ( n == 1) return List.of(new int[]{from, to});
 
         int empty = 6 - from - to;
-        List<int[]> result = new ArrayList<>();
         result.addAll(hanoi(n-1, from, empty));
         result.addAll(hanoi(1, from, to));
         result.addAll(hanoi(n-1, empty, to));
@@ -31,7 +32,8 @@ public class Hanoi3 {
     }
 
     public int[][] solution(int n) {
-        return hanoi(n, 1, 3).toArray(new int[0][]);
+    List<int[]> result = hanoi(n, 1, 3);
+    return result.toArray(new int[0][]);
     }
 }
 
