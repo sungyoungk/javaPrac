70c2d25bd514577c290856dbde93132191a5575f
sungyoung.kim <sungyoung.kim@medisolution.o.kr>
2025-02-20 15:54:10 +0900

    [복습] 카펫 - 와 헷갈린다

--- src/algorithmPrac/exhaustiveSearch/carpet/Carpet.java
+++ src/algorithmPrac/exhaustiveSearch/carpet/Carpet.java
@@ -18,22 +18,21 @@ public class Carpet {
 //   • 갈색 격자의 수 brown은 8 이상 5,000 이하인 자연수입니다.
 //   • 노란색 격자의 수 y이low는 1 이상 2,000,000 이하인 자연수입니다.
 //   • 카펫의 가로 길이는 세로 길이와 같거나, 세로 길이보다 깁니다.
-
-
-
     int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
 
-        for ( int width = 3; width <= 5000; width++) {
-            for( int height = 3; height <= width; height ++) {
-                int boundary = (width + height -2) * 2;
-                int center = (width * height) - boundary;
+        for (int width = 3; width < 5000; width++) {
+            for ( int height = 3; height <= width; height++) {
+                int boundary = (width + height - 2) * 2;
+                int center = width + height - boundary;
 
-                if(boundary == brown && center == yellow) {
-                    return new int[]{width, height};
+                if (center == yellow && boundary == brown) {
+                    answer[0] = width;
+                    answer[1] = height;
+                    break;
                 }
             }
         }
-        return null;
+        return answer;
     }
-
 }
