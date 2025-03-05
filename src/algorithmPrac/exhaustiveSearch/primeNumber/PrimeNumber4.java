ca1338ae8e270e2a1fc631c8afc1e6ace48a10af
Sung Young Kim <skim8700@gmail.com>
2025-03-05 11:30:15 +0900

    [복습] 소수 찾기 - 언제쯤 재귀가 편해질까?

--- src/algorithmPrac/exhaustiveSearch/primeNumber/PrimeNumber4.java
+++ src/algorithmPrac/exhaustiveSearch/primeNumber/PrimeNumber4.java
@@ -1,6 +1,9 @@
 package algorithmPrac.exhaustiveSearch.primeNumber;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
@@ -11,51 +14,42 @@ public class PrimeNumber4 {
 
     /***
      *  @재귀_구성하기
-     *  @상태 ( acc, nums)
-     *  @종료_조건 ( acc, 0)
-     *  @점화식 ( acc, {})
+     *  @상태 (acc, nums)
+     *  @종료_조건 (acc, 0)
+     *  @점화식 (acc, { })
      *      소수인 경우 (acc * 10 + nums(i), nums -1)
      *      아닌경우 -
      */
 
-    private boolean isPrime(int number) {
-        if (number <= 1) return false;
-
-        for(int i = 2; i * i <= number; i++) {
-            if (number % i == 0) return false;
+    private boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
         }
         return true;
     }
 
-    private Set<Integer> getPrimes(int acc, List<Integer> numbers) {
+    private Set<Integer> getPrimes(int acc, List<Integer> nums) {
         Set<Integer> primes = new HashSet<>();
-        if(isPrime(acc)) primes.add(acc);
+        if (isPrime(acc)) primes.add(acc); // 소수면 primes Set 에 넣기
 
-        for (int i = 0; i < numbers.size(); i ++) {
-            int nextNum = acc * 10 + numbers.get(i);
-
-            List<Integer> nums = new ArrayList<>(numbers);
-            nums.remove(i);
-            primes.addAll(getPrimes(nextNum, nums));
+        for (int i = 0; i < nums.size(); i++) {
+            int nextAcc = acc * 10 + nums.get(i);
+            List<Integer> nextNums = new ArrayList<>(nums);
+            nextNums.remove(i);
+            primes.addAll(getPrimes(nextAcc, nextNums));
         }
         return primes;
     }
 
     public int solution(String numbers) {
-    List<Integer> nums = numbers.chars().map(c -> c - '0')
-                                        .boxed()
-                                        .collect(Collectors.toList());
-
-    Set<Integer> answer = getPrimes(0, nums);
-
-    return answer.size();
-    }
-
-    public static void main(String[] args) {
-        PrimeNumber4 prime = new PrimeNumber4();
-        prime.solution("17");
-
-        System.out.println(prime.solution("17"));
+        List<Integer> nums = numbers.chars()
+                .map(c -> c - '0')
+                .boxed()
+                .collect(Collectors.toList());
+        Set<Integer> primes = new HashSet<>();
+        return getPrimes(0, nums).size();
     }
 
 }
+
