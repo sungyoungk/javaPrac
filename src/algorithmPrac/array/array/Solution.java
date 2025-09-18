77d8ad4466e798eee1a34093b83b8ad573d2a815
Sung Young Kim <skim8700@gmail.com>
2025-09-18 11:01:52 +0900

    교점에 별찍기 p.71

--- src/algorithmPrac/array/array/Solution.java
+++ src/algorithmPrac/array/array/Solution.java
@@ -4,83 +4,91 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-class Solution {
+public class Solution {
     public String[] solution(int[][] line) {
-    List<Point> points = new ArrayList<>();
-    for(int i = 0; i < line.length; i++) {
-        for(int j = i + 1; j < line.length; j++) {
-            Point intersection = intersection(line[i][0], line[i][1], line[i][2],
-                                              line[j][0], line[j][1], line[j][2]);
+        List<Point> points = new ArrayList<>();
+        for (int i = 0; i < line.length; i++) {
+            for (int j = 0; j < line[i].length; j++) {
+                Point intersection = intersection(line[i][0], line[i][1], line[i][2],
+                        line[j][0], line[j][1], line[j][2]);
 
-            if(intersection != null) {
-                points.add(intersection);
+                if (intersection != null) points.add(intersection);
             }
         }
-    }
 
-        Point maximum = getMaximum(points);
-        Point minimum = getMinimum(points);
+        Point max = getMax(points);
+        Point min = getMin(points);
 
-        int width =  (int) (maximum.x - minimum.x + 1);
-        int height = (int) (maximum.y - minimum.y + 1);
+        int width = (int) (max.x - min.x + 1);
+        int height = (int) (max.y - min.y + 1);
 
         char[][] arr = new char[height][width];
-        for(char[] row : arr) {
+        for (char[] row : arr) {
             Arrays.fill(row, '.');
         }
 
         for (Point p : points) {
-            int x = (int) (p.x - minimum.x);
-            int y = (int) (maximum.y - p.y);
+            int x = (int) (p.x - min.x);
+            int y = (int) (max.y - p.y);
             arr[y][x] = '*';
         }
 
         String[] result = new String[arr.length];
-        for(int i = 0; i < result.length; i++) {
+        for (int i = 0; i < result.length; i++) {
             result[i] = new String(arr[i]);
         }
         return result;
     }
 
-    private static class Point {
-        public final long x;
-        public final long y;
+    private class Point {
+        public final long x, y;
 
-        public Point(long x, long y) {
+        Point(long x, long y) {
             this.x = x;
-            this.y = y;}
+            this.y = y;
+        }
     }
 
     private Point intersection(long a1, long b1, long c1, long a2, long b2, long c2) {
-        double x = (double)(b1 * c2 - b2 * c1) / (a1 * b2 - a2 * b1);
-        double y = (double)(a2 * c1 - a1 * c2) / (a1 * b2 - a2 * b1);
+        double x = (double) (b1 * c2 - c1 * b2) / (a1 * b2 - b1 * a2);
+        double y = (double) (c1 * a2 - a1 * c2) / (a1 * b2 - b1 * a2);
 
-        if((x % 1 != 0 || y % 1 != 0) ){
-            return null;
-        }
-        return new Point((long)x, (long)y);
+        if (x % 1 != 0 || y % 1 != 0) return null;
+
+        return new Point((long) x, (long) y);
     }
 
-    private Point getMinimum(List<Point> points) {
-        long x = Long.MAX_VALUE;
-        long y = Long.MAX_VALUE;
-
-        for(Point p : points) {
-            if(p.x < x) x = p.x;
-            if(p.y < y) y = p.y;
-        }
-        return new Point(x, y);
-    }
-
-    private Point getMaximum(List<Point> points) {
+    private Point getMax(List<Point> points) {
         long x = Long.MIN_VALUE;
         long y = Long.MIN_VALUE;
 
-        for(Point p : points) {
-            if(p.x > x) x = p.x;
-            if(p.y > y) y = p.y;
+        for (Point p : points) {
+            if (x < p.x) x = p.x;
+            if (y < p.y) y = p.y;
         }
         return new Point(x, y);
     }
 
+    private Point getMin(List<Point> points) {
+        long x = Long.MAX_VALUE;
+        long y = Long.MAX_VALUE;
+
+        for (Point p : points) {
+            if (x > p.x) x = p.x;
+            if (y > p.y) y = p.y;
+        }
+        return new Point(x, y);
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = new int[][]{{1, -1, 4}, {-2, -1, 4}, {0, -1, 1}, {5, -8, -12}, {5, 8, 12}};
+
+        Solution solution = new Solution();
+        String[] answer = solution.solution(arr);
+
+        for(String str : answer) {
+            System.out.println(str);
+        }
+    }
 }
+
