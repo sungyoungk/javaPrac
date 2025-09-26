cf0347b143e62beca5f67c83bcae6abefb4d42b7
Sung Young Kim <skim8700@gmail.com>
2025-09-26 14:48:47 +0900

    교점에 별찍기 p.71-에러남

--- src/algorithmPrac/array/array/Solution.java
+++ src/algorithmPrac/array/array/Solution.java
@@ -6,89 +6,86 @@ import java.util.List;
 
 public class Solution {
     public String[] solution(int[][] line) {
-        List<Point> points = new ArrayList<>();
+
+        List<Point> list = new ArrayList<>();
+
         for (int i = 0; i < line.length; i++) {
-            for (int j = 0; j < line[i].length; j++) {
+            for (int j = i + 1; j < line.length; j++) {
                 Point intersection = intersection(line[i][0], line[i][1], line[i][2],
                         line[j][0], line[j][1], line[j][2]);
 
-                if (intersection != null) points.add(intersection);
+                if (intersection != null) list.add(intersection);
             }
         }
 
-        Point max = getMax(points);
-        Point min = getMin(points);
+        Point max = getMax(list);
+        Point min = getMin(list);
 
-        int width = (int) (max.x - min.x + 1);
-        int height = (int) (max.y - min.y + 1);
+        int width  = (int)(max.x - min.x + 1);
+        int height = (int)(max.x - min.x + 1);
 
         char[][] arr = new char[height][width];
-        for (char[] row : arr) {
-            Arrays.fill(row, '.');
+        for (char[] chars : arr) {
+            Arrays.fill(chars, '.');
         }
 
-        for (Point p : points) {
-            int x = (int) (p.x - min.x);
-            int y = (int) (max.y - p.y);
+        for (Point p : list) {
+            int x = (int)(p.x - min.x);
+            int y = (int)(max.y - p.y);
             arr[y][x] = '*';
         }
 
         String[] result = new String[arr.length];
-        for (int i = 0; i < result.length; i++) {
+        for ( int i = 0; i < result.length; i++) {
             result[i] = new String(arr[i]);
         }
+
         return result;
     }
 
-    private class Point {
-        public final long x, y;
+    private static class Point {
+        private final long x;
+        private final long y;
 
-        Point(long x, long y) {
+        public Point(long x, long y) {
             this.x = x;
             this.y = y;
         }
     }
 
-    private Point intersection(long a1, long b1, long c1, long a2, long b2, long c2) {
-        double x = (double) (b1 * c2 - c1 * b2) / (a1 * b2 - b1 * a2);
-        double y = (double) (c1 * a2 - a1 * c2) / (a1 * b2 - b1 * a2);
+    Point intersection(long a, long b, long e,
+                       long c, long d, long f) {
 
-        if (x % 1 != 0 || y % 1 != 0) return null;
+        double x = (double) ((b * f - e * d) / (a * d - b * c));
+        double y = (double) ((e * c - a * f) / (a * d - b * c));
+
+        if (x % 1 != 0 && y % 1 != 0) {
+            return null;
+        }
 
         return new Point((long) x, (long) y);
     }
 
-    private Point getMax(List<Point> points) {
+    Point getMax(List<Point> list) {
         long x = Long.MIN_VALUE;
         long y = Long.MIN_VALUE;
-
-        for (Point p : points) {
-            if (x < p.x) x = p.x;
-            if (y < p.y) y = p.y;
+        for (Point p : list) {
+            if (p.x > x) x = p.x;
+            if (p.y > y) y = p.y;
         }
         return new Point(x, y);
     }
 
-    private Point getMin(List<Point> points) {
+    Point getMin(List<Point> list) {
         long x = Long.MAX_VALUE;
         long y = Long.MAX_VALUE;
 
-        for (Point p : points) {
-            if (x > p.x) x = p.x;
-            if (y > p.y) y = p.y;
+        for (Point p : list) {
+            if (p.x < x) x = p.x;
+            if (p.y < y) y = p.y;
+
         }
         return new Point(x, y);
-    }
-
-    public static void main(String[] args) {
-        int[][] arr = new int[][]{{1, -1, 4}, {-2, -1, 4}, {0, -1, 1}, {5, -8, -12}, {5, 8, 12}};
-
-        Solution solution = new Solution();
-        String[] answer = solution.solution(arr);
-
-        for(String str : answer) {
-            System.out.println(str);
-        }
     }
 }
 
