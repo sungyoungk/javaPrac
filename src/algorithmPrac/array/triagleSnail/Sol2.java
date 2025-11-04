c0133c7614966e61aaf288d70b5371ea83d7d0d9
Sung Young Kim <skim8700@gmail.com>
2025-11-04 17:31:44 +0900

    삼각달팽이  p.72 - 행의 갯수와 답안 배열의 갯수를 헷갈림
    https://school.programmers.co.kr/learn/courses/30/lessons/68645

--- src/algorithmPrac/array/triagleSnail/Sol2.java
+++ src/algorithmPrac/array/triagleSnail/Sol2.java
@@ -12,51 +12,47 @@ public class Sol2 {
         int y = 0;
 
         while (true) {
-            // 아래로 이동
-            while (true) {
+
+            while(true) {
                 triangle[y][x] = v++;
                 if (y + 1 == n || triangle[y + 1][x] != 0) break;
                 y += 1;
             }
-            if (x + 1 == n || triangle[y][x + 1] != 0) break;
+            if(x + 1 == n || triangle[y][x + 1] != 0) break;
             x += 1;
 
-            // 오른쪽으로 이동
             while (true) {
                 triangle[y][x] = v++;
-                if (x + 1 == n || triangle[y][x + 1] != 0) break;
+                if (x + 1 == n || triangle[y][x+1] != 0) break;
                 x += 1;
             }
-            if (triangle[y - 1][x - 1] != 0) break;
-            x -= 1;
-            y -= 1;
+            if (triangle[y -1][x -1] != 0) break;
+            y -=1;
+            x -=1;
 
-            //왼쪽 위로 이동
             while (true) {
                 triangle[y][x] = v++;
-                if (triangle[y-1][x-1] != 0) break;
-                x -= 1;
+                if (triangle[y -1][x -1] != 0) break;
                 y -= 1;
+                x -= 1;
             }
-            if (y + 1 == n || triangle[y + 1][x] != 0) break;
+            if ( y +1 == n || triangle[y +1][x] != 0) break;
             y += 1;
+            // 반복문 마지막
         }
 
-        int[] result = new int[v - 1];
+        int[] result = new int[v -1];
         int index = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
                 result[index++] = triangle[i][j];
             }
         }
-        return result;
-    }
-
-    public static void main(String[] args) {
-        Sol2 solution = new Sol2();
-        System.out.println(Arrays.toString(solution.solution(6)));
-
+            return result;
+        }
     }
 
 
-}
+
+
+
